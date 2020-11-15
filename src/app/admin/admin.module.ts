c4c21c6d0d7f1730e304ebf9b43abeb1e687d60d
mohamedmadian10 <Mohamed.Nabil@eplhouse.com>
2020-11-15 18:03:43 +0200

    add dynamic modal for creating and editing user

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { UserCreateComponent } from './users/user-create/user-create.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { NgxSpinnerModule } from "ngx-bootstrap-spinner";
-import {ModalModule} from "ngx-bootstrap/modal"
+import {ModalModule} from "ngx-bootstrap/modal";
+import { UserCreateModalComponent } from './users/user-create-modal/user-create-modal.component'
 
 @NgModule({
     declarations:[
         UserCreateComponent,
-        UserListComponent
+        UserListComponent,
+        UserCreateModalComponent
     ],
     imports:[CommonModule,AdminRoutingModule, FormsModule, NgxSpinnerModule, ReactiveFormsModule,ModalModule.forChild()],
     exports:[AdminRoutingModule,]
